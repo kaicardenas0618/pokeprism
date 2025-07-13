d93880a32b7b8d03ab78d7b7106f57245ec37e50
kaicardenas0618 <kcardenasit@gmail.com>
2025-07-13 19:38:45 -0400

    Fix Cancel and Confirm Button Windows

--- src/party_menu.c
+++ src/party_menu.c
@@ -2432,7 +2432,7 @@ static void CreateCancelConfirmWindows(bool8 chooseHalf)
         if (gPartyMenu.menuType != PARTY_MENU_TYPE_SPIN_TRADE)
         {
             mainOffset = GetStringCenterAlignXOffset(FONT_SMALL, gText_Cancel, 48);
-            AddTextPrinterParameterized3(cancelWindowId, FONT_SMALL, mainOffset + offset, 1, sFontColorTable[0], TEXT_SKIP_DRAW, gText_Cancel);
+            AddTextPrinterParameterized3(cancelWindowId, FONT_SMALL, 9, 0, sFontColorTable[0], TEXT_SKIP_DRAW, gText_Cancel);
         }
         else
         {
