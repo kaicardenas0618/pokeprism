57fea3d8d64753cff2bc4d82ed221d52629ae853
kaicardenas0618 <kcardenasit@gmail.com>
2025-07-17 11:40:48 -0400

    Namebox (Currently Broken)

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1009,6 +1009,10 @@ void SetMapVarsToTrainerA(void)
         gSpecialVar_LastTalked = TRAINER_BATTLE_PARAM.objEventLocalIdA;
         gSelectedObjectEvent = GetObjectEventIdByLocalIdAndMap(TRAINER_BATTLE_PARAM.objEventLocalIdA, gSaveBlock1Ptr->location.mapNum, gSaveBlock1Ptr->location.mapGroup);
     }
+    if (TRAINER_BATTLE_PARAM.opponentA != 0)
+    {
+        gSpeakerName = gTrainers[TRAINER_BATTLE_PARAM.opponentA]->trainerName;
+    }
 }
 
 void SetMapVarsToTrainerB(void)
@@ -1017,6 +1021,10 @@ void SetMapVarsToTrainerB(void)
     {
         gSpecialVar_LastTalked = TRAINER_BATTLE_PARAM.objEventLocalIdB;
         gSelectedObjectEvent = GetObjectEventIdByLocalIdAndMap(TRAINER_BATTLE_PARAM.objEventLocalIdB, gSaveBlock1Ptr->location.mapNum, gSaveBlock1Ptr->location.mapGroup);
+    }
+    if (TRAINER_BATTLE_PARAM.opponentB != 0)
+    {
+        gSpeakerName = gTrainers[TRAINER_BATTLE_PARAM.opponentB]->trainerName;
     }
 }
 
@@ -1488,9 +1496,15 @@ static const u8 *ReturnEmptyStringIfNull(const u8 *string)
 static const u8 *GetIntroSpeechOfApproachingTrainer(void)
 {
     if (gApproachingTrainerId == 0)
+    {
+        gSpeakerName = gTrainers[TRAINER_BATTLE_PARAM.opponentA]->trainerName;
         return ReturnEmptyStringIfNull(TRAINER_BATTLE_PARAM.introTextA);
+    }
     else
+    {
+        gSpeakerName = gTrainers[TRAINER_BATTLE_PARAM.opponentB]->trainerName;
         return ReturnEmptyStringIfNull(TRAINER_BATTLE_PARAM.introTextB);
+    }
 }
 
 const u8 *GetTrainerALoseText(void)
