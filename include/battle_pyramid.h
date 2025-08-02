0146c1b99686eec66867a934ab912d8ed205635c
kaicardenas0618 <150625530+kaicardenas0618@users.noreply.github.com>
2025-08-01 23:31:29 -0400

    Expansion 1.12.2 (#72)
    * Improve Pyramid location documentaion
    
    * Stick to the new name
    
    * Applied review changes
    
    * Fix other missing instances of MAX_LINK_PLAYERS
    
    * Apply fixes to preproc for Windows compatibility
    
    * Relax MAPSEC region map data requirements
    
    * Fix sGpuRegBuffer potential alignment issues (#2157)
    
    * Ensure sMonMarkings_Gfx alignment (#2158)
    
    * Add explicit defaults for MAPSEC data
    
    * Fix RL compressed data alignment
    
    `RLUnComp`, like `LZ77UnComp`, expects the source data to be word-aligned.
    So, to mirror how lz-compressed data is defined as `u32[]` to enforce
    word-alignment, this changes rl-compressed data to be defined as `u32[]`
    to enforce word-alignment.
    
    * Adds Missing break in ABILITY_TERAFORM_ZERO (#7377)
    
    * Fix potential stack memory leak in gbagfx due to out of bounds read (#2162)
    
    * fix(tools): gbagfx potential oob read
    
    ---------
    
    Co-authored-by: sbird <sbird@no.tld>
    
    * Add pack/unpack for data masks (#2163)
    
    * Fix Incompatible Z-Move Activation (#7399)
    
    Co-authored-by: ghoulslash <pokevoyager0@gmail.com>
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>
    
    * Fixes bug for affectionScore in doubles for spread moves (#7395)
    
    * Fixes Wandering Spirit overwriting battlers (#7407)
    
    * Fix hit escape switching hardcode (#7403)
    
    * document shop menu palette
    
    * Make B_VAR_STARTING_STATUS tests run in CI (#7419)
    
    * Fix Echoing Voice calcs (#7416)
    
    * Fixes Razor Wind not activating after hit effects after the first target (#7413)
    
    * Changed incorrect comment
    
    * Fixes Effect Spore and Baneful Bunker/Burning Bulwark (#7412)
    
    * 1.12.2 release version
    
    * Start of 1.12.3 cycle
    
    * add tustin2121 as a contributor for doc, and code (#7445)
    
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    
    * Fixes Magnet Rise when user is Rooted/Smacked Down (#7449)
    
    * Avoid doubling up on status moves in double battles. (#7452)
    
    * Adds AI_FLAG_ASSUMPTIONS as a constant for semi-omniscience (#7435)
    
    * Additional effects tweak (#7392)
    
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>
    
    * Wild Pokémon animate after breaking out of Poké Ball (#7339)
    
    * Replace InBattlePyramid_ With InBattlePyramid
    
    ---------
    
    Co-authored-by: Eduardo Quezada <eduardo602002@gmail.com>
    Co-authored-by: Catnip-Consumer <mira.kankaanpera@gmail.com>
    Co-authored-by: GriffinR <griffin.g.richards@gmail.com>
    Co-authored-by: DizzyEggg <jajkodizzy@wp.pl>
    Co-authored-by: Raymond Dodge <git@rayrobdod.name>
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>
    Co-authored-by: Philipp AUER <SBird1337@users.noreply.github.com>
    Co-authored-by: sbird <sbird@no.tld>
    Co-authored-by: ghoulslash <41651341+ghoulslash@users.noreply.github.com>
    Co-authored-by: ghoulslash <pokevoyager0@gmail.com>
    Co-authored-by: Pawkkie <61265402+Pawkkie@users.noreply.github.com>
    Co-authored-by: cawtds <38510667+cawtds@users.noreply.github.com>
    Co-authored-by: surskitty <surskitty@gmail.com>
    Co-authored-by: PhallenTree <168426989+PhallenTree@users.noreply.github.com>
    Co-authored-by: Hedara <hedara90@gmail.com>
    Co-authored-by: allcontributors[bot] <46447321+allcontributors[bot]@users.noreply.github.com>
    Co-authored-by: AlexOn1ine <alexthenotes@gmail.com>
    Co-authored-by: Martin Griffin <martinrgriffin@gmail.com>
    Co-authored-by: Nephrite <thechurchofcage@gmail.com>
    Co-authored-by: Linathan <35115312+LinathanZel@users.noreply.github.com>

--- include/battle_pyramid.h
+++ include/battle_pyramid.h
@@ -1,14 +1,16 @@
 #ifndef GUARD_BATTLE_PYRAMID_H
 #define GUARD_BATTLE_PYRAMID_H
 
+#include "constants/battle_pyramid.h"
+
 void CallBattlePyramidFunction(void);
 u16 LocalIdToPyramidTrainerId(u8 localId);
 bool8 GetBattlePyramidTrainerFlag(u8 eventId);
 void MarkApproachingPyramidTrainersAsBattled(void);
 void GenerateBattlePyramidWildMon(void);
 u8 GetPyramidRunMultiplier(void);
-u8 InBattlePyramid(void);
-bool8 InBattlePyramid_(void);
+u8 CurrentBattlePyramidLocation(void);
+bool8 InBattlePyramid(void);
 void PausePyramidChallenge(void);
 void SoftResetInBattlePyramid(void);
 void CopyPyramidTrainerSpeechBefore(u16 trainerId);
