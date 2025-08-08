396adad18ff2846d95e264d65c2698a12949e7e3
kaicardenas0618 <150625530+kaicardenas0618@users.noreply.github.com>
2025-08-07 21:04:03 -0400

    Custom Starter Case (#78)

---
+++ include/ui_starter_choose.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_UI_STARTER_CHOOSE_H
+#define GUARD_UI_STARTER_CHOOSE_H
+
+#include "main.h"
+
+void Task_OpenStarterChoose(u8 taskId);
+void StarterChoose_Init(MainCallback callback);
+
+#endif // GUARD_UI_STARTER_CHOOSE_H
